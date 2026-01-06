e451b9dfe4847bd1e33a0710f7564edfdd7d4d19
nimabar13901390-rgb <nimabar13901390@gmail.com>
2026-01-06 17:34:11 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,26 @@
+# تنظیمات اصلی
+CONFIG = {
+    "BOT_TOKEN": "568312051:OXMkUvzFqtSnWov0YdBfNJXnd3sWUzXsoUU",
+    "CHANNEL": "@hoshmasnoye",
+    
+    # رقبا برای تحلیل
+    "COMPETITORS": [
+        "@ai_bot_images",
+        "@athenaai",
+        "@asrgooyeshpardaz",
+        "@aminiacademyeducation",
+        "@Aieducational"
+    ],
+    
+    # تنظیمات زمانی
+    "POST_SCHEDULE": {
+        "hours": [9, 12, 16, 20, 22],
+        "interval_minutes": 60
+    },
+    
+    # تنظیمات یادگیری
+    "LEARNING": {
+        "max_posts_to_analyze": 50,
+        "min_virality_score": 0.7
+    }
+}
